ce9ef821f6134bafc5c1da56f67e6ce1969555b4
tandie oyucho <ysoyucho@gmail.com>
2025-03-08 15:42:19 +0300

    added changes

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper'
+
+const welcomeMessage = `Welcome to Flatbook, Grace Hopper!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, G! ${currentUser.slice(0, 1)}!`;
+
+console.log(welcomeMessage); // Output: Welcome to Flatbook, Grcae Hopper!
+console.log(excitedWelcomeMessage); // Output: WELCOME TO FLATBOOK, GRACE HOPPER!
+console.log(shortGreeting); // Output: Welcome G,!
